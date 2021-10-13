bbb975e9845422d68a6787ca228d8c689f77d7b2
Jayavardhan Alumuru <jayavardhanram@gmail.com>
2021-10-13 13:49:36 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div >
+    <EmployeeList/>
   </div>
-  <router-view/>
+ 
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+//import HelloWorld from './components/HelloWorld.vue'
+import EmployeeList from './components/EmployeeList.vue'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: { EmployeeList},
+  name :"App"
+};
+</script>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="EmployeeList">
+    <h1>
+      Employee list
+    </h1>
+    <table border="1px">
+      <tr>
+       <td>Id</td>
+       <td>Name</td>
+       <td>Salary</td>
+       <td>Age</td>
+      </tr>
+
+      <tr v-for="item in list" v-bind:key="item.id">
+       <td>{{item.id  }} </td> 
+       <td>{{item.employee_name }} </td>
+       <td>{{item.employee_salary}} </td>
+       <td>{{item.employee_age}} </td>
+      </tr>
+      
+    </table>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+
+export default {
+  name : "EmployeeList",
+  data(){
+    return {list: undefined}
+  },
+  mounted(){
+    axios.get('http://dummy.restapiexample.com/api/v1/employees').then((response)=>{
+      this.list=response.data.data
+      console.warn(response.data.data)
+    })
+  }
+}
+</script>
